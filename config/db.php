7d992d347e906b968328fa8536aa845ce3248186
JuanDev3 <58000053+JuanDev3@users.noreply.github.com>
2022-07-22 16:31:29 -0500

    Edicion formularios

--- config/db.php
+++ config/db.php
@@ -2,11 +2,11 @@
 
 class Database{
     public static function StartUp(){        
-        $contraseña = 'usersql$$nisira';
-        $usuario = 'sa';
-        $nombreBaseDeDatos = 'NSFAJA';
+        $contraseña = '';
+        $usuario = '';
+        $nombreBaseDeDatos = '';
         # Puede ser 127.0.0.1 o el nombre de tu equipo; o la IP de un servidor remoto - \\MSSQLSERVER, 1433
-        $rutaServidor = '192.168.1.7';        
+        $rutaServidor = '';        
         $db = new PDO("sqlsrv:Server=$rutaServidor;Database=$nombreBaseDeDatos", $usuario, $contraseña);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $db;
