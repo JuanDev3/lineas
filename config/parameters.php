21ce22e9ae83129de69c5d30c6be214a20458462
JuanDev3 <58000053+JuanDev3@users.noreply.github.com>
2022-07-22 16:32:26 -0500

    Actualizar formulario login

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define("base_url","http://192.168.3.84:8080/lineas/");
+define("base_url","http://192.168.1.1/lineas/");
 //define("base_url", "https:///");
 define("controller_default", "homeController");
 define("action_default", "index");
